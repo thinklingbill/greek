e046d046f351f3bd1c1d7fdaecf48a59afbef1fa
Bill Roberts <bill.roberts@billrobertsconsulting.com>
2019-12-08 19:25:49 -0600

    Initial commit

---
+++ schema/greek/seed_data/contraction.sql
@@ -0,0 +1,2 @@
+INSERT INTO `greek`.`contraction` (`firstElement`, `secondElement`, `genuineSpurious`, `result`) VALUES ('α', 'α', 'G', 'α');
+INSERT INTO `greek`.`contraction` (`firstElement`, `secondElement`,`genuineSpurious`, `result`) VALUES ('α', 'ε', 'G', 'α');
---
+++ schema/greek/tables/contraction.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS greek.contraction;
+
+CREATE TABLE `greek`.`contraction` (
+  `contractionId` INT NOT NULL AUTO_INCREMENT,
+  `firstElement` VARCHAR(10) NOT NULL,
+  `secondElement` VARCHAR(10) NOT NULL,
+  `genuineSpurious` CHAR(1) NOT NULL,
+  `result` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`contractionId`));
+  
+ALTER TABLE `greek`.`contraction` 
+ADD UNIQUE INDEX `ixEntry` USING BTREE (`firstElement` ASC, `secondElement` ASC, `genuineSpurious` ASC);
+  
