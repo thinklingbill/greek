7cc0ee648b6c1f488d18e0b7c8c7bc3075309660
Bill Roberts <bill.roberts@billrobertsconsulting.com>
2020-04-27 08:19:53 -0500

    Wiping the slate clean

--- services/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   /* connection info */
-   $_serverName = "localhost";
-   $_userName = "root";
-   $_password = "8Do2mic2)E";
-   $_dbName = "greek";
-?>
--- services/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-   define( "FAILURE", "FAIL" );
-   define( "SUCCESS", "OK" );
-?>
